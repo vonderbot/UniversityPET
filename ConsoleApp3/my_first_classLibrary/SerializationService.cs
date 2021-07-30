59cc4aabab0f781e945d45a8462d57ad9e0ad284
Богдан Тимошицкий <vonderbot1@gmail.com>
2021-07-30 17:17:56 +0300

    Defined serialization service.

---
+++ ConsoleApp3/my_first_classLibrary/SerializationService.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace University.dll
+{
+    public class SerializationService<T>
+    {
+        public string Serialize(T obj) => JsonConvert.SerializeObject(obj);
+        public T Deserialize(string buffer) => JsonConvert.DeserializeObject<T>(buffer);
+    }
+}
